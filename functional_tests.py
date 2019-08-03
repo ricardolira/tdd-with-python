4e8e7ac8384e8e2faa0415df7f8c26c6f9844032
ricardolira <r.liraf@gmail.com>
2019-08-03 10:21:53 -0300

    Refactor home page view to use template

--- functional_tests.py
+++ functional_tests.py
@@ -11,8 +11,6 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-
-
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage.
@@ -23,7 +21,6 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-
         # She is invited to enter a to-do item straight away.
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),
@@ -33,15 +30,15 @@ class NewVisitorTest(unittest.TestCase):
         # is tying fly-fishing lures).
         inputbox.send_keys('Buy peacock feathers')
 
-
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list.
         inputbox.send_keys(Keys.RETURN)
 
         table = self.browser.find_element_by_id('id_new_item')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers.' for row in rows))
-
+        self.assertTrue(any(row.text == '1: Buy peacock feathers.'
+                            for row in rows)
+                        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical).
@@ -59,5 +56,5 @@ class NewVisitorTest(unittest.TestCase):
 
 # Satisfied, she goes back to sleep.
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     unittest.main(warnings='ignore')
