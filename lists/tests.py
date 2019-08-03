7f75525e7a6e40f4d05718d9f4b7038f7d8b1efb
ricardolira <r.liraf@gmail.com>
2019-08-03 13:24:33 -0300

    "Major changes in tests. Refactor FT."

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.shortcuts import render
 
 from .views import home_page
 
@@ -14,7 +15,22 @@ class HomepageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+
+        expected_html = render_to_string('home.html', request=request)
+
+        self.assertTrue(response.content.decode(), expected_html)  # originally assertEqual for django 1
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html',
+                                         {'new_item_text': 'A new list itm'})
+        self.assertTrue(response.content.decode(), expected_html)  # originally assertIn for django 1
